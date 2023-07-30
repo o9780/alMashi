2f5d4ebc4bb6123a1c2a55173c0934bef3de55eb
o9780 <o1000500@gmail.com>
2023-07-30 15:57:23 +0300

    edit rate pass code

--- src/component/rate_pass.js
+++ src/component/rate_pass.js
@@ -14,11 +14,11 @@ const App = () => {
     StatusBar.setHidden(true);
   return(
     <RatePage 
-    // Rate={<Rate st={Styles.text} text={"Rate Passenger"}/>}
-    // MainButton={<MainButton buttonStyle={[Styles.signButtonStyle,{top:400},{right:103}]}button={"Submit"}/>}
+    Rate={<Rate st={Styles.text} text={"Rate Passenger"}/>}
+    MainButton={<MainButton buttonStyle={[Styles.signButtonStyle,{top:400},{right:103}]}button={"Submit"}/>}
      MainTextField ={<MainTextField textInput={[Styles.textInput,{backgroundColor:"green"}]} />}
-    // BackArrow={<BackArrow style={{resizeMode:"contain",opacity:0.9,width:30,bottom:400,left:10}}/>}
-    // Bell={<Bell style={{opacity:0.9, bottom:660, width:30, left:353,height:30}}/>}
+    BackArrow={<BackArrow style={{resizeMode:"contain",opacity:0.9,width:30,bottom:400,left:10}}/>}
+    Bell={<Bell style={{opacity:0.9, bottom:660, width:30, left:353,height:30}}/>}
   />
   );
   }
